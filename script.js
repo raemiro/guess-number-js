d69df53b1adbd0b50d945e6ebf7e404bcaed55ca
Ramiro <marioramiro0888@gmail.com>
2026-02-22 21:59:17 -0500

    feat: lógica base del juego con número aleatorio

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ const btnReiniciar = document.getElementById('btnReiniciar');
 const tarjeta = document.getElementById('game-card');
 
 console.log("Elementos conectados:", inputIntento, btnAdivinar);
+// 2️⃣ Variables del juego
+let numeroSecreto = Math.floor(Math.random() * 100) + 1;
+let intentos = 0;
+let historialIntentos = [];
+
+console.log("(DEBUG) Número secreto:", numeroSecreto);
